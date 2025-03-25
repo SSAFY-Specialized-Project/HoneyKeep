5b14c80273a183c3e5b08573cd07160c4d07a4ed
Seonghun, Bae <bsh7937@naver.com>
2025-03-25 09:26:44 +0900

    refactor: 고정 지출 관련 피드백 적용

--- server/back/src/main/java/com/barcode/honeykeep/fixedexpense/repository/FixedExpenseRepository.java
+++ server/back/src/main/java/com/barcode/honeykeep/fixedexpense/repository/FixedExpenseRepository.java
@@ -7,6 +7,6 @@ import java.util.List;
 
 public interface FixedExpenseRepository extends JpaRepository<FixedExpense, Long> {
 
-    List<FixedExpense> findByUserId(Integer userId);
+    List<FixedExpense> findByUserId(Long userId);
 
 }
