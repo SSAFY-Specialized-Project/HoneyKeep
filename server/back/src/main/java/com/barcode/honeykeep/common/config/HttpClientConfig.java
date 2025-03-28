31b2a442da8bc19eaeff32a91df5bcaf44132f99
Seonghun, Bae <bsh7937@naver.com>
2025-03-29 04:50:24 +0900

    refactor: 파일 구조 수정

--- server/back/src/main/java/com/barcode/honeykeep/common/config/HttpClientConfig.java
+++ server/back/src/main/java/com/barcode/honeykeep/common/config/HttpClientConfig.java
@@ -2,15 +2,21 @@ package com.barcode.honeykeep.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class WebClientConfig {
+public class HttpClientConfig {
 
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
                 .baseUrl("https://finopenapi.ssafy.io/ssafy/api/v1")
                 .build();
+    }
+
+    @Bean
+    public RestClient restClient(RestClient.Builder builder) {
+        return builder.build();
     }
 }
--- server/back/src/main/java/com/barcode/honeykeep/common/config/WebConfig.java
+++ server/back/src/main/java/com/barcode/honeykeep/common/config/WebConfig.java
@@ -2,7 +2,10 @@ package com.barcode.honeykeep.common.config;
 
 import com.barcode.honeykeep.common.interceptor.ApiLoggingInterceptor;
 import com.barcode.honeykeep.common.interceptor.RateLimitInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -27,5 +30,23 @@ public class WebConfig implements WebMvcConfigurer {
         // 일단 모든 API 요청에 Rate Limit 적용
         registry.addInterceptor(rateLimitInterceptor)
                 .addPathPatterns("/api/**");
+    }
+
+    @Bean
+    public RedisScript<Long> rateLimiterScript() {
+        String script =
+                "local key = KEYS[1] " +
+                        "local limit = tonumber(ARGV[1]) " +
+                        "local window = tonumber(ARGV[2]) " +
+                        "local current = redis.call('get', key) " +
+                        "if current and tonumber(current) > limit then " +
+                        "   return tonumber(current) " +
+                        "end " +
+                        "current = redis.call('incr', key) " +
+                        "if tonumber(current) == 1 then " +
+                        "   redis.call('expire', key, window) " +
+                        "end " +
+                        "return tonumber(current)";
+        return new DefaultRedisScript<>(script, Long.class);
     }
 }
