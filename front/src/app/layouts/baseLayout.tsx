e50c9bbe784aafd4b66cdc6c967e7805782ce2da
jump6746 <jump6746@naver.com>
2025-04-04 18:05:10 +0900

    feat: header store 생성

--- front/src/app/layouts/baseLayout.tsx
+++ front/src/app/layouts/baseLayout.tsx
@@ -1,10 +1,19 @@
-import { Layout } from "@/shared/ui";
-import { BasicHeader } from "@/widgets/header/ui";
-import { GlobalNavigation } from "@/widgets/navigation/ui";
+import { usePocketUseModalStore } from '@/shared/store';
+import { Layout } from '@/shared/ui';
+import { BasicHeader } from '@/widgets/header/ui';
+import { PocketUseModal } from '@/widgets/modal/ui';
+import { GlobalNavigation } from '@/widgets/navigation/ui';
 
 const BaseLayout = () => {
+  const { isOpen: pocketModalOpen, modalProps: pocketModalProps } = usePocketUseModalStore();
   return (
-    <Layout headerSlot={<BasicHeader />} navbarSlot={<GlobalNavigation />} />
+    <Layout
+      headerSlot={<BasicHeader />}
+      navbarSlot={<GlobalNavigation />}
+      modalSlot={
+        <PocketUseModal isOpen={pocketModalOpen} pocketId={pocketModalProps?.pocketId ?? 0} />
+      }
+    />
   );
 };
 
