0be23f8057011a57e7b6ad29efe947e68e8b7d0b
Seonghun, Bae <bsh7937@naver.com>
2025-04-10 22:08:22 +0900

    fix: 버그 수정

--- front/src/shared/store/index.ts
+++ front/src/shared/store/index.ts
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-export { default as useLoadingStore } from "./useLoadingStore";
-export { default as useBasicModalStore} from "./useBasicModalStore";
-export { default as usePocketUseModalStore } from "./usePocketUseModalStore";
-export { default as useHeaderStore} from "./useHeaderStore";
-export { default as useGatheringModalStore} from "./useGatheringModalStore";
-export { default as useFCMStore} from "./useFCMStore";
+
 export { default as usePocketChooseStore } from "./usePocketChooseStore";
-=======
 export { default as useLoadingStore } from './useLoadingStore';
 export { default as useBasicModalStore } from './useBasicModalStore';
 export { default as usePocketUseModalStore } from './usePocketUseModalStore';
@@ -14,4 +7,3 @@ export { default as useHeaderStore } from './useHeaderStore';
 export { default as useGatheringModalStore } from './useGatheringModalStore';
 export { default as useFCMStore } from './useFCMStore';
 export { default as useNoticeModalStore } from './useNoticeModalStore';
->>>>>>> a86a081ecc7df8c214a21c19d8b135e7a28490bd
